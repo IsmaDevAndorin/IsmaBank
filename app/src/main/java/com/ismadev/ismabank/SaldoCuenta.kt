298f6fea3f8c723d880a1c161da4f5abb31d4dce
IsmaDevBalmis <75784444+IsmaDevBalmis@users.noreply.github.com>
2021-12-13 01:51:09 +0100

    Añado cambios en saldo cuenta

--- app/src/main/java/com/ismadev/ismabank/SaldoCuenta.kt
+++ app/src/main/java/com/ismadev/ismabank/SaldoCuenta.kt
@@ -1,13 +1,18 @@
 package com.ismadev.ismabank
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
+
 class SaldoCuenta : AppCompatActivity() {
     private val db = FirebaseFirestore.getInstance()
+    private val details = ArrayList<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_saldo_cuenta)
@@ -17,6 +22,8 @@ class SaldoCuenta : AppCompatActivity() {
 
         //Load Data
         loadData()
+
+
 
     }
 
@@ -28,12 +35,30 @@ class SaldoCuenta : AppCompatActivity() {
 
     private fun loadData(){
         var saldoTextView = findViewById<TextView>(R.id.saldoEnCuentaTextView)
+        var listaDetalleSaldo = findViewById<ListView>(R.id.listaDetalleSaldo)
+
 
         var email = FirebaseAuth.getInstance().currentUser?.email.toString()
 
         db.collection("users").document(email).get().addOnSuccessListener {
+            var saldo = (it.get("saldoEnCuenta") as String?)!!.toInt()
+
+            if(saldo >= 0){
+            saldoTextView.setTextColor(Color.GREEN)
+            }else{
+                saldoTextView.setTextColor(Color.RED)
+            }
             saldoTextView.setText((it.get("saldoEnCuenta") as String?) + "€")
         }
+
+        details.add("Operacion 1")
+        details.add("Operacion 2")
+        details.add("Operacion 3")
+
+        val adapter: ArrayAdapter<String> =
+            ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, details)
+
+        listaDetalleSaldo.setAdapter(adapter);
     }
 
 }
